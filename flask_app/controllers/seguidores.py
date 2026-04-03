645b5e5dd9ada89fa7fb6d82808d40fdd00d5154
Tomás González <tomi@Tomass-MacBook-Pro.local>
2026-04-03 13:37:27 +0200

    feat(controllers): add controller seguidores

---
+++ flask_app/controllers/seguidores.py
@@ -0,0 +1,40 @@
+from flask import render_template, redirect, request, session, flash
+from flask_app import app
+from flask_app import bcrypt
+from flask_app.models.seguidor import Seguidor
+from flask_app.models.grupo import Grupo
+
+@app.route('/seguir_usuario/<int:usuario_id>')
+def seguir_usuario(usuario_id):
+    if 'usuario_id' not in session:
+        return redirect('/')
+    datos = {
+        'usuario_seguidor_id': session['usuario_id'],
+        'usuario_seguido_id': usuario_id
+    }
+    Seguidor.seguir(datos)
+    return redirect(f'/mis_seguidores')
+
+@app.route('/mis_seguidores')
+def mis_seguidores():
+    if 'usuario_id' not in session:
+        return redirect('/')
+    datos = {
+        'usuario_id': session['usuario_id']
+    }
+    seguidores = Seguidor.obtener_seguidores(datos)
+    seguidos = Seguidor.obtener_seguidos(datos)
+    # Obtener grupos para el menú desplegable
+    mis_grupos = Grupo.obtener_grupos_por_usuario(datos)
+    return render_template('mis_seguidores.html', seguidores=seguidores, seguidos=seguidos, mis_grupos=mis_grupos)
+
+@app.route('/dejar_seguir/<int:usuario_id>')
+def dejar_seguir(usuario_id):
+    if 'usuario_id' not in session:
+        return redirect('/')
+    datos = {
+        'usuario_seguidor_id': session['usuario_id'],
+        'usuario_seguido_id': usuario_id
+    }
+    Seguidor.dejar_de_seguir(datos)
+    return redirect('/mis_seguidores')
